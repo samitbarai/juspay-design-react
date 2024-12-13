84b64dae2c80df61cb0cf073357766fbeec518e5
samitbarai <samit.barai@juspay.in>
2024-12-13 17:19:12 +0530

    Some more changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Hi Samit Barai!</h1>
     <p>Welcome to your new Gatsby site. This is our first website built using React</p>
-    <p>Now go build something great.</p>
+
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
