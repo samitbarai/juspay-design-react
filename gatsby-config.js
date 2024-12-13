853263701b28e87390e2a21de32ff3bbc148720d
Samit Barai <samit.barai@SAMIT-BARAI-HNYFH1HWL0.local>
2024-12-13 17:05:14 +0530

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Design Team React Course',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Hi Samit Barai!</h1>
+    <p>Welcome to your new Gatsby site. This is our first website built using React</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
