5a1c7a5573417748ea808fa796a7116c6e58dc96
samitbarai <samit.barai@juspay.in>
2024-12-16 12:06:32 +0530

    Chapter 2 done

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi Samit Barai!</h1>
-    <p>Welcome to your new Gatsby site. This is our first website built using React</p>
-
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className='Hero'>
+      <div className='HeroGroup'>
+        <h1>Hi Samit Barai!</h1>
+        <p>Welcome to your new Gatsby site. This is our first website built using React</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
+   
   </div>
 )
 
